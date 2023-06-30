2a4de3ea0b2daef9ec79b2c8f9472676d0773d51
Eduardo <jocdwardo25@gmail.com>
2023-06-30 17:00:11 -0500

    update placeholder phone

--- app/page.tsx
+++ app/page.tsx
@@ -235,7 +235,7 @@ const HomePage = () => {
                 type="tel"
                 id="phone"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="123-45-678"
+                placeholder="987-654-321"
                 name="phone"
                 required
                 onChange={handleChange}
